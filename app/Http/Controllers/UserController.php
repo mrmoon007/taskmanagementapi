cb001892bd3cfe9d1590a774e769dfe429ab8310
mostafijur007 <mostafijur.techvill@gmail.com>
2023-08-29 22:41:00 +0600

    [refactor] Changed method name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
      *     ),
      * )
      */
-    public function createUser()
+    public function index()
     {
         return $this->successResponse([]);
     }
